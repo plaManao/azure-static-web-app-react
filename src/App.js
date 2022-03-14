3fb1088727f52ae7dcd019f3dc3c15a738b8fc0c
Arnuparb jumfoo <arnuparb,jumfoo@gmail.com>
2022-03-14 18:31:39 +0700

    call APIs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/GetContacts?name=John`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
